95bcd4f07dc42635f3f3c304e8de27f2e6057dcc
Dan Dubois <74828217+schematicdnb@users.noreply.github.com>
2025-02-15 15:27:14 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Carls-Cars/',
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-      },
-    },
-  },
 });
